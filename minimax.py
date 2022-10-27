b6fc720c25fc263711eb8eae1307d32cac37414b
Ayoub-Vip <ayoub.assaoud@student.liege.be>
2022-10-27 22:20:53 +0200

    making heuristic faster and changing the new files source of this yearg status

--- minimax.py
+++ minimax.py
@@ -1,31 +1,37 @@
 from pacman_module.game import Agent
 from pacman_module.game import Directions
-import pacman_module.util as util
-import sys
 
-sys.setrecursionlimit(1500)
-
-def Utility(currentstate):
-    return currentstate.getScore()
-
-def Terminal_Test(currentstate,depth,maxdepth):
-    return (currentstate.isWin() or currentstate.isLose() or depth >= maxdepth)
-
-def key(state):
-    """Returns a key that uniquely identifies a Pacman game state.
-
-    Arguments:
-        state: a game state. See API or class `pacman.GameState`.
-
-    Returns:
-        A hashable key tuple.
-    """
-
-    return state.getPacmanPosition(), state.getFood(), state.getGhostPosition(1)
 
 class PacmanAgent(Agent):
-    def __init__(self,args):
-        self.max_depth = 10
+    def __Utility(self, state):
+        return state.getScore()
+
+    def __Terminal_Test(self, state):
+        return (state.isWin() or state.isLose())
+
+    def __key(self, state):
+        """Returns a key that uniquely identifies a Pacman game state.
+
+        Arguments:
+            state: a game state. See API or class `pacman.GameState`.
+
+        Returns:
+            A hashable key tuple.
+        """
+        tup = tuple([state.getPacmanPosition(), 
+            state.getFood(), tuple(state.getCapsules())])
+
+        for i in range(state.getNumAgents() - 1):
+            tup = tup + (state.getGhostPosition(i + 1), state.getScore())
+        return tup
+
+    def __init__(self):
+        """
+        Arguments:
+        ----------
+        - `alltime_closed`: initiation of intermediate states dictionary 
+        """
+        self.alltime_closed = dict()
 
     def get_action(self, state):
         """
@@ -38,70 +44,54 @@ class PacmanAgent(Agent):
         -------
         - A legal move as defined in `game.Directions`.
         """
-        return self.minimax(state)
+        curKey = self.__key(state)
+        self.alltime_closed[curKey] = self.minimax(state)
+        return self.alltime_closed[curKey]
 
-       
     def minimax(self, state):
-        visited = dict()
+        def max_value(currentstate, depth):
+            curKey = self.__key(currentstate)
 
-        def max_value(currentstate, depth, alpha, beta):
-            curKey = key(currentstate)
-
-            if Terminal_Test(currentstate, depth, self.max_depth):
-                return  Utility(currentstate)
-            elif curKey in visited:   # Prevent cycles
-                return visited[curKey]
+            if self.__Terminal_Test(currentstate):
+                return  self.__Utility(currentstate)
+            elif curKey in self.alltime_closed:
+                return float("-inf")
 
             best_action = Directions.STOP
             best_score = float("-inf")
 
             for result, action in currentstate.generatePacmanSuccessors():
-                score = min_value(result, 1, depth, alpha, beta)
+                score = min_value(result, 1, depth)
                 if score > best_score:
                     best_score = score
                     best_action = action
-                #prunning
-                # alpha =max(alpha,best_score)
-                # if best_score >= beta:
-                #     visited[curKey]=best_score
-                #     return best_score
             if depth == 0:
-                visited[curKey]=best_action
                 return best_action
-            visited[curKey]=best_score
+
             return best_score
 
-        def min_value(currentstate, ghostIndex, depth, alpha, beta):
+        def min_value(currentstate, ghostIndex, depth):
+            curKey = self.__key(currentstate)
 
-            if Terminal_Test(currentstate, depth, self.max_depth):
-                return  Utility(currentstate)
-            curKey = key (currentstate)
-            if curKey in visited:
-                return visited[curKey]
+            if self.__Terminal_Test(currentstate):
+                return  self.__Utility(currentstate)
+            elif curKey in self.alltime_closed:
+                return float("inf")
             next_player = ghostIndex + 1
 
             if ghostIndex == currentstate.getNumAgents() - 1:
                 next_player = 0
 
             best_score = float("inf")
-
-            for result, action in currentstate.generateGhostSuccessors(ghostIndex):
+            succesors = currentstate.generateGhostSuccessors(ghostIndex)
+            for result, action in succesors:
                 if next_player == 0:
-                    score= max_value(result,depth+1,alpha,beta)
+                    score = max_value(result, depth + 1)
                 else:
-                    score= min_value(result,next_player,depth,alpha,beta)
-                best_score =min(best_score,float(score))
-                # prunning
-                # beta = min(beta, best_score)
-                # if best_score <= alpha:
-                #     visited[curKey]=best_score
-                #     return best_score
-            visited[curKey]=best_score
+                    score = min_value(result, next_player, depth)
+                best_score =min(best_score, float(score))
+
             return best_score
 
-        
-        return  max_value(state,0,float("-inf"),float("inf"))
-  
-
-
-    
+        maxv = max_value(state, 0)
+        return maxv
